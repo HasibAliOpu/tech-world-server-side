0c97262e83e4b708f5995d9a636cc9d02d8cc482
H_A _Opu <wwwhasib4@gmail.com>
2022-04-30 23:05:54 +0600

    get item on mongodb using get api on run function

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
 require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(express.json);
+app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hvm8i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+const itemCollection = client.db("techWorld").collection("items");
+
+async function run() {
+  try {
+    await client.connect();
+    app.get("/item", async (req, res) => {
+      const cursor = itemCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+run();
 
 app.get("/", (req, res) => {
   res.send("Server is Running");
