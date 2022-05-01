a836825d3ca04f28bd82b0d51497db78749b4719
H_A _Opu <wwwhasib4@gmail.com>
2022-05-01 12:36:25 +0600

    create POST API for add item

--- index.js
+++ index.js
@@ -20,18 +20,31 @@ const itemCollection = client.db("techWorld").collection("items");
 async function run() {
   try {
     await client.connect();
-    // get all item
+
+    // GET ALL ITEM
     app.get("/item", async (req, res) => {
       const cursor = itemCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-    // get by id
+
+    // GET ITEM BY ID
     app.get("/item/:id", async (req, res) => {
       const id = req.params.id;
       const item = await itemCollection.findOne({ _id: ObjectId(id) });
 
       res.send(item);
+    });
+
+    // POST
+    app.post("/item", async (req, res) => {
+      const newItem = req.body;
+
+      await itemCollection.insertOne(newItem);
+      res.send({
+        success: true,
+        message: `${newItem.name} Successfully Added`,
+      });
     });
   } catch (error) {
     console.log(error);
