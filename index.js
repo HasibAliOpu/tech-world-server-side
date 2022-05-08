ae35ffd0f801076201363a9f3305a0fc08559539
H_A _Opu <wwwhasib4@gmail.com>
2022-05-08 10:29:24 +0600

    setup JWT email verify function and using middletare on myItem API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,21 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hvm8i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -20,6 +36,14 @@ async function run() {
   try {
     await client.connect();
 
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.SECRET_TOKEN, {
+        expiresIn: "7d",
+      });
+      res.send({ accessToken });
+    });
+
     // GET ALL ITEM
     app.get("/item", async (req, res) => {
       const cursor = itemCollection.find();
@@ -28,11 +52,16 @@ async function run() {
     });
 
     // GET API for find by email
-    app.get("/myItem", async (req, res) => {
+    app.get("/myItem", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
-      const cursor = itemCollection.find({ email });
-      const result = await cursor.toArray();
-      res.send(result);
+      if (decodedEmail === email) {
+        const cursor = itemCollection.find({ email });
+        const result = await cursor.toArray();
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "Forbidden Access" });
+      }
     });
 
     // GET ITEM BY ID
